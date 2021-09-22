e552cb1440b3de8383fff2dfac254518c54b15fb
Thiago Henrique Martins da Silva <thiagohenrique1508@gmail.com>
2021-09-22 20:39:46 -0300

    Commit das aulas "Services", "Listas Assíncronas", "Criando um Component" e "Passando os dados para o Component".

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { LoginPageComponent } from './pages/account/login-page/login-page.component';
 import { PetsPageComponent } from './pages/account/pets-page/pets-page.component';
 import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
@@ -11,6 +12,7 @@ import { SignupPageComponent } from './pages/account/signup-page/signup-page.com
 import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
 import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
 import { FramePageComponent } from './pages/master/frame.page';
+import { ProductCardComponent } from './components/store/product-card/product-card.component';
 
 @NgModule({
   declarations: [
@@ -23,8 +25,9 @@ import { FramePageComponent } from './pages/master/frame.page';
     ProductsPageComponent,
     CartPageComponent,
     FramePageComponent,
+    ProductCardComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/store/products-page/products-page.component.ts
+++ src/app/pages/store/products-page/products-page.component.ts
@@ -1,11 +1,17 @@
+import { DataService } from './../../../services/data.service';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-products-page',
   templateUrl: './products-page.component.html',
 })
 export class ProductsPageComponent implements OnInit {
-  constructor() {}
+  public products$: Observable<any[]> | undefined;
 
-  ngOnInit(): void {}
+  constructor(private data: DataService) {}
+
+  ngOnInit(): void {
+    this.products$ = this.data.getProducts();
+  }
 }
