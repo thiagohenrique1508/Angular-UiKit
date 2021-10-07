bd8ce065422093964a0c25d710eab32ce592aa77
Thiago Henrique Martins da Silva <thiagohenrique1508@gmail.com>
2021-10-06 21:25:57 -0300

    Commit das aulas "Edição de Perfil: Parte 1" e "Edição de Perfil: Parte 2"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,21 +8,26 @@ import { FramePageComponent } from './pages/master/frame.page';
 import { PetsPageComponent } from './pages/account/pets-page/pets-page.component';
 import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
 import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
+import { ProfilePageComponent } from './pages/account/profile-page/profile-page.component';
 
 const routes: Routes = [
   {
     path: '',
     component: FramePageComponent,
+    canActivate: [AuthService],
     children: [
       { path: '', component: ProductsPageComponent },
-      { path: 'cart', component: CartPageComponent, canActivate: [AuthService], }
-      // { path: 'checkout', component: checkoutPageComponent, canActivate: [AuthService] },
+      { path: 'cart', component: CartPageComponent }
+      // { path: 'checkout', component: checkoutPageComponent },
     ],
   },
   {
     path: 'account',
     component: FramePageComponent,
-    children: [{ path: 'pets', component: PetsPageComponent }],
+    children: [
+      { path: '', component: ProfilePageComponent },
+      { path: 'pets', component: PetsPageComponent }
+    ],
   },
   { path: 'login', component: LoginPageComponent },
   { path: 'signup', component: SignupPageComponent },
