c2571fecf3f52995e53d7ae6f8a85805f9b0ae4f
Thiago Henrique Martins da Silva <thiagohenrique1508@gmail.com>
2021-10-10 08:20:01 -0300

    Commit das aulas "Adicionando Itens ao Carrinho", "Manipulando o Carrinho" e " Visualizando o Carrinho"

--- src/app/components/store/product-card/product-card.component.ts
+++ src/app/components/store/product-card/product-card.component.ts
@@ -1,15 +1,31 @@
-import { Product } from './../../../models/product.model';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { CartUtil } from 'src/app/utils/cart.util';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-product-card',
-  templateUrl: './product-card.component.html',
+  templateUrl: './product-card.component.html'
 })
 export class ProductCardComponent implements OnInit {
-  @Input()
-  product: Product;
+  @Input() product: Product;
 
-  constructor() {}
+  constructor(
+    private toastr: ToastrService
+  ) { }
 
-  ngOnInit(): void {}
+  ngOnInit() {
+  }
+
+  addToCart() {
+    CartUtil.add(
+      this.product._id,
+      this.product.title,
+      1,
+      this.product.price,
+      this.product.images[0]
+    )
+    this.toastr.success(`${this.product.title} adicionado ao carrinho`, 'Produto Adicionado');
+  }
+
 }
